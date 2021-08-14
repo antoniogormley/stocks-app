49d6dd5c6da9a6b9d07428ea608bb870b6c32dd7
antoniogormley <antoniogormley@gmail.com>
2021-08-14 01:23:40 +0100

    Initial Commit

---
+++ Stocks App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stocks App
+//
+//  Created by Antonio Gormley on 14/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stocks App/Stocks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stocks_AppApp.swift
+//  Stocks App
+//
+//  Created by Antonio Gormley on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Stocks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
